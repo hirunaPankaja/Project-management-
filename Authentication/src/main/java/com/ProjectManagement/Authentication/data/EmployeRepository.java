0afdf548dc272e27977ed659d7c6e9d0bd49db68
L.Hiruna Pankaja <164976900+hirunaPankaja@users.noreply.github.com>
2024-11-28 22:36:06 +0530

    Delete Authentication directory

--- Authentication/src/main/java/com/ProjectManagement/Authentication/data/EmployeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ProjectManagement.Authentication.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface EmployeRepository extends JpaRepository<Employer,String> {
-    Optional<Employer> findByEmail(String email);
-}
